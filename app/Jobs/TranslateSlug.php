4e52bcf75ea9985635ef73b161ba062c9d74871f
memory_123 <haomani521@126.com>
2018-12-13 17:31:33 +0800

    编辑器添加，话题编辑，删除

---
+++ app/Jobs/TranslateSlug.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Library\Translate;
+use App\Models\Topic;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class TranslateSlug implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    protected  $topic;
+    public function __construct(Topic $topic)
+    {
+        //
+        $this->topic=$topic;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $slug=app(Translate::class)->translate($this->topic->title);
+
+        // 为了避免模型监控器死循环调用，我们使用 DB 类直接对数据库进行操作
+        \DB::table('topics')->where('id', $this->topic->id)->update(['slug' => $slug]);
+    }
+}
