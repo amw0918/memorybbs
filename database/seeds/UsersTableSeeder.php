89d79739756c6495ea84695bd1f16e98ad967acb
memory_123 <haomani521@126.com>
2018-12-13 14:06:57 +0800

    新增话题

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 生成数据集合
+        $users = factory(User::class)
+            ->times(10)
+            ->make();
+
+        // 让隐藏字段可见，并将数据集合转换为数组
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        // 插入到数据库中
+        User::insert($user_array);
+
+        $user=User::find(1);
+        $user->email="387982650@qq.com";
+        $user->save();
+    }
+}
